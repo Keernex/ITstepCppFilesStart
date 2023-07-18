ba2b4254169905ebfdea3fd490cae4717c34bedc
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-07-18 15:45:35 +0300

    no done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	FileWork file;
+	FileWork fileWork;
 	int count = 3;
 	string* users = new string[count]{"admin", "vladyslav", "alexey"};
 	string* passwords = new string[count]{ "adminadmin", "12345678", "alexey" };
-	file.Write(users, passwords, count);
+	fileWork.Write(users, passwords, count);
 	IntroScreen::Show();
 	return 0;
 }
