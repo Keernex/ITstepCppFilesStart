1556d1113a51587a674f4c620af3cf66386de3be
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-07-25 09:32:13 +0300

    no done

--- FileProviderEmployees.h
+++
@@ -1,8 +0,0 @@
-#include "Employees.h"
-
-struct FileProviderEmployees
-{
-	fstream file;
-	bool save_employee(vector<Employees> employees, int count);
-	bool load_employee(vector<Employees>& employees, int& count);
-};
